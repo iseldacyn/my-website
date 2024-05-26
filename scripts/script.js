42caa09c31a1c6b31c15f12dc3c1737164056a5e
iselda aiello <aielloise@gmail.com>
2024-05-25 23:09:33 -0400

    desktop navbar scroll

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,28 @@ function toggleHamburger(x)
 {
 	x.classList.toggle( "change" )
 }
+
+/* make navbar scroll up and down on scroll */
+var prev_scroll = window.pageYOffset;
+window.onscroll = function() {
+	if( window.innerWidth > 1300 )
+	{
+		var current_scroll = window.pageYOffset;
+		if( prev_scroll >= current_scroll )
+		{
+			document.getElementById( "navigation_bar" ).style.top = "0";
+			document.getElementById( "navigation_bar" ).style.transition = "top 0.4s ease-in";
+		}
+		else
+		{
+			document.getElementById( "navigation_bar" ).style.top = "-100px";
+			document.getElementById( "navigation_bar" ).style.transition = "top 0.4s ease-out";
+		}
+		prev_scroll = current_scroll;
+	}
+	else
+	{
+		document.getElementById( "navigation_bar" ).style.top = "0";
+		document.getElementById( "navigation_bar" ).style.transition = "top 0.4s ease-in";
+	}
+}
